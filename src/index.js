93eb464ad95bc1683fbc2a2b2a52ed54cf1ac276
maksat_sagandykov <maksssat@gmail.com>
2021-03-01 01:30:53 +0600

    webpack config

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "/styles/main.css";
+
+// async function f() {
+//   let promise = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("готово!"), 1000);
+//   });
+
+//   let result = await promise; // будет ждать, пока промис не выполнится (*)
+
+//   alert(result); // "готово!"
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,159 @@
 const path = require("path");
+const isProductionMode =
+  process.argv[process.argv.indexOf("--mode") + 1] === "production";
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+// const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+// const CopyPlugin = require("copy-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: isProductionMode ? "production" : "development",
+
+  devtool: isProductionMode ? false : "inline-source-map",
+
+  target: isProductionMode ? "browserslist" : "web",
+
+  context: path.resolve(__dirname, "src"),
+
+  entry: {
+    index: "./index.js",
+  },
+
   output: {
-    filename: "bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  plugins: [
+    new ESLintPlugin(),
+    new HTMLWebpackPlugin({
+      template: "./index.html",
+      inject: "body",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style/[name].[contenthash].css",
+      chunkFilename: "[id].css",
+    }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     {
+    //       from: path.resolve(__dirname, "src", "images"),
+    //       to: "./images/", //[name].[contenthash].[ext]
+    //     },
+    //   ],
+    // }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+
+      //for css import in html
+      // {
+      //   test: /\.css$/i,
+      //   use: [
+      //     {
+      //       loader: "postcss-loader",
+      //       options: {
+      //         sourceMap: true,
+      //         postcssOptions: {
+      //           plugins: [
+      //             [
+      //               "autoprefixer",
+      //               {
+      //                 // Options
+      //               },
+      //             ],
+      //             "postcss-import",
+      //           ],
+      //         },
+      //       },
+      //     },
+      //   ],
+      //   generator: {
+      //     filename: "styles/[name].[contenthash].css",
+      //   },
+      // },
+
+      // for css import in js
+      {
+        test: /\.css$/i,
+        use: [
+          isProductionMode
+            ? MiniCssExtractPlugin.loader
+            : {
+                loader: "style-loader",
+                options: {},
+              },
+          { loader: "css-loader", options: { sourceMap: true } },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              postcssOptions: {
+                plugins: [
+                  [
+                    "autoprefixer",
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset",
+        generator: {
+          filename: isProductionMode
+            ? "images/[name].[contenthash][ext]"
+            : "images/[name][ext]",
+        },
+      },
+
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  useBuiltIns: "usage",
+                  corejs: 3,
+                  shippedProposals: true,
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+
+  devServer: {
+    watchContentBase: true,
+    contentBase: path.join(__dirname, "src"),
+    open: true,
+    hot: true,
   },
 };
