574a8aa457e39b1903999216a38f6baeaba709c3
maksat_sagandykov <maksssat@gmail.com>
2021-03-05 02:12:52 +0600

    переключение тем

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "/styles/main.css";
+
+const switcher = document.querySelector("#theme-switcher");
+
+switcher.addEventListener("change", () => {
+  const html = document.querySelector("html");
+  html.classList.toggle("dark");
+});
